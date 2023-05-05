a9f5b3301314a77f5eca435463b3828b7b1770b6
Ryoichi Kakehi <ryoichikakehi@gmail.com>
2023-05-06 02:36:55 +0900

    modified:   GLCM.py 	new file:   classifier.py

--- GLCM/img/GLCM.py
+++ GLCM/img/GLCM.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import glob
 import traceback
 
 import numpy as np
@@ -11,14 +12,17 @@ from skimage.feature import graycomatrix, graycoprops
 from skimage.measure import block_reduce, shannon_entropy
 from PIL import PpmImagePlugin
 
+from sklearn import preprocessing
+
 
 class GLCM():
 
     def __init__(self) -> None:
         self.__FIN = None
+        self.__LABEL = None
         self.__PATCH_SIZE = 35
         self.__LEVELS = 4096
-        self.__DISTANCES = [1]
+        self.__DISTANCES = [1, 3, 5]
         self.__ANGLES = [0, np.pi/4, np.pi/2, 3*np.pi/4]
 
     @property
@@ -28,6 +32,14 @@ class GLCM():
     @fin.setter
     def fin(self, fin:str):
         self.__FIN = fin
+
+    @property
+    def label(self):
+        return self.__LABEL
+
+    @label.setter
+    def label(self, label:int):
+        self.__LABEL = label
 
     @property
     def PATCH_SIZE(self):
@@ -112,13 +124,19 @@ class GLCM():
             plt.show()
 
 
-    def calc_glcm(self, pooled=False) -> None:
+    def calc_glcm(self, pooled=False) -> list:
 
         def _calc_entropy(glcm:object) -> float:
-            entropy = 0
-            for i, angle in enumerate(self.ANGLES):
-                entropy += shannon_entropy(self.GLCM[:, :, 0, i])
-            return entropy / len(self.ANGLES)
+            entropies = []
+            for i, distance  in enumerate(self.DISTANCES):
+                entropy = 0
+                for j, angle in enumerate(self.ANGLES):
+                    entropy += shannon_entropy(self.GLCM[:, :, i, j])
+
+                # 各Distanceは角度別データの平均値とする
+                entropies.append(entropy / len(self.DISTANCES))
+
+            return entropies
 
         if pooled:
             img = self.pooled_img
@@ -130,20 +148,26 @@ class GLCM():
                                  self.ANGLES,
                                  levels=self.levels)
 
-        disssimilarity = graycoprops(self.GLCM, 'dissimilarity')[0, 0]
-        correlation    = graycoprops(self.GLCM, 'correlation')[0, 0]
-        energy         = graycoprops(self.GLCM, 'energy')[0, 0]
-        homogeneity    = graycoprops(self.GLCM, 'homogeneity')[0, 0]
-        contrast       = graycoprops(self.GLCM, 'contrast')[0, 0]
+        disssimilarity = graycoprops(self.GLCM, 'dissimilarity').mean(axis=1)
+        correlation    = graycoprops(self.GLCM, 'correlation').mean(axis=1)
+        energy         = graycoprops(self.GLCM, 'energy').mean(axis=1)
+        homogeneity    = graycoprops(self.GLCM, 'homogeneity').mean(axis=1)
+        contrast       = graycoprops(self.GLCM, 'contrast').mean(axis=1)
         entropy        = _calc_entropy(self.GLCM)
 
-        print("%s,%f,%f,%f,%f,%f,%f\n"
-              %(self.fin, disssimilarity, correlation,
-                energy, homogeneity, contrast, entropy))
+        DSS = [str(disssimilarity[i]) for i in range(len(disssimilarity))]
+        COR = [str(correlation[i])    for i in range(len(correlation))]
+        ENG = [str(energy[i])         for i in range(len(energy))]
+        HMG = [str(homogeneity[i])    for i in range(len(homogeneity))]
+        CTR = [str(contrast[i])       for i in range(len(contrast))]
+        ENT = [str(entropy[i])        for i in range(len(entropy))]
+        
+        print(self.fin, self.label,
+              ",".join(DSS+COR+ENG+HMG+CTR+ENT), sep=",")
 
 
     def apply_pooling(self, 
-                method:str='max', 
+                method:str='average', 
                 new_levels:int=256, 
                 padding_size:int=0,
                 padding_mode:str='edge') -> None:
@@ -153,7 +177,6 @@ class GLCM():
                        mode:str='edge') -> np.array:
 
             padded_img = np.pad(img, pad_width=size, mode=mode)
-            # padded_img = np.pad(img, pad_width=size, mode=mode)
             return padded_img
 
         if padding_size > 0:
@@ -165,19 +188,23 @@ class GLCM():
         # 画像サイズを所望のものとすることが出来る
         if method == 'max':
             self.pooled_img = block_reduce(img[padding_size:-padding_size,
-                                               padding_size:-padding_size],
+                                               padding_size:-padding_size]\
+                                               if padding_size > 0 else img,
                                            (2, 2), np.max)
         elif method == 'min':
             self.pooled_img = block_reduce(img[padding_size:-padding_size,
-                                               padding_size:-padding_size],
+                                               padding_size:-padding_size]\
+                                               if padding_size > 0 else img,
                                            (2, 2), np.min)
         elif method == 'average':
             self.pooled_img = block_reduce(img[padding_size:-padding_size,
-                                               padding_size:-padding_size],
+                                               padding_size:-padding_size]\
+                                               if padding_size > 0 else img,
                                            (2, 2), np.mean)
         elif method == 'median':
             self.pooled_img = block_reduce(img[padding_size:-padding_size,
-                                               padding_size:-padding_size],
+                                               padding_size:-padding_size]\
+                                               if padding_size > 0 else img,
                                            (2, 2), np.median)
         else:
             raise ValueError("Invalid pooling method. Choose from 'max', 'min', 'average', or 'median'.")
@@ -207,16 +234,30 @@ class GLCM():
             self.imshow(vmin=vmin, vmax=vmax, save_img=save_img)
 
 
-    def run_all(self) -> None:
+    def run_all(self,
+                pooling=False,
+                padding_size=0, save_img=False,
+                vmin:float=0, vmax:float=100) -> None:
+
         try:                             
-            for root, dirs, files in os.walk(os.getcwd()):
-                files.sort(key=lambda x: os.path.getmtime(x))
-                for fin in files:
-                    suffix = os.path.splitext(fin)[1]
-                    if suffix == '.pgm':
-                        self.glcm(fin)
-                    else:
-                        continue
+            for directory_path in glob.glob("train/*"):
+                label = directory_path.split("/")[-1]
+                self.label = label
+                for img_path in glob.glob(os.path.join(directory_path, "*.pgm")):
+                    self.run(img_path,
+                             pooling=pooling,
+                             padding_size=padding_size,
+                             save_img=save_img,
+                             vmin=vmin, vmax=vmax)
+
+            #for root, dirs, files in os.walk(os.getcwd()):
+            #    files.sort(key=lambda x: os.path.getmtime(x))
+            #    for fin in files:
+            #        suffix = os.path.splitext(fin)[1]
+            #        if suffix == '.pgm':
+            #            self.glcm(fin)
+            #        else:
+            #            continue
 
         except Exception as e:
             print(e)
---
+++ GLCM/img/classifier.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score 
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+import lightgbm as lgb
+#from lightgbm import plot_importance, plot_metric
+
+
+class MachineLearningModel():
+
+    def __init__(self, csv_file:str, seed:int=44):
+        names = ("csv_file", "type", 
+                 "DSS1", "DSS3", "DSS5",
+                 "COR1", "COR3", "COR5",
+                 "ENG1", "ENG3", "ENG5",
+                 "HMG1", "HMG3", "HMG5",
+                 "CTR1", "CTR3", "CTR5",
+                 "ENT1", "ENT3", "ENT5")
+        self.data = pd.read_csv(csv_file, names=names)
+        self.seed = seed
+        self.encoder = LabelEncoder()
+        self.model = None
+
+
+    def preprocess_data(self):
+        self.data['type'] = self.encoder.fit_transform(self.data['type'])
+        self.train_data, self.test_data =\
+            train_test_split(self.data,
+                             test_size=0.2,
+                             random_state=self.seed)
+
+
+    def train(self) -> None:
+        # Make training data
+        X_train = self.train_data.drop(columns=['csv_file', 'type'])
+        y_train = self.train_data['type']
+        lgb_train = lgb.Dataset(X_train, y_train)
+
+        # Add evaluation data for plot_metric()
+        X_test = self.test_data.drop(columns=['csv_file', 'type'])
+        y_test = self.test_data['type']
+        lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
+
+        params = {
+            'objective': 'multiclass',
+            'num_class': len(self.encoder.classes_),
+            'metric': 'multi_logloss',
+            'verbosity': 1
+        }
+
+        # Record metrics during training
+        evals_result = {}
+        self.model = lgb.train(
+            params,
+            lgb_train,
+            valid_sets=[lgb_train, lgb_eval],
+            evals_result=evals_result
+        )
+
+        self.evals_result = evals_result
+        #return evals_result
+
+    def predict(self, data):
+        return self.encoder.inverse_transform(
+            self.model.predict(data, num_iteration=self.model.best_iteration)
+        )
+
+
+    def evaluate(self) -> float:
+        X_test = self.test_data.drop(columns=['csv_file', 'type'])
+        y_test = self.test_data['type']
+        y_pred = self.model.predict(X_test).argmax(axis=1)
+        #y_true = self.encoder.inverse_transform(y_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        return accuracy
+        #return classification_report(y_test, y_pred)
+        #return classification_report(y_true, y_pred)
+
+
+    def show_summary(self):
+        # show the summary of trainning
+        print(f"Training Summary:")
+        print(f"Training data size: {len(self.train_data)}")
+        print(f"Test data size: {len(self.test_data)}")
+        print(f"Number of classes: {len(self.encoder.classes_)}")
+
+        # show the sum
+        accuracy = self.evaluate()
+        y_test = self.test_data['type']
+        y_pred = self.model.predict(
+            self.test_data.drop(columns=['csv_file', 'type'])
+        ).argmax(axis=1)
+        print(f'\nAccuracy: {accuracy:.2f}')
+        print(f'\nClassification Report:')
+        print(classification_report(y_test, y_pred))
+        print(f'\nConfusion Matrix:')
+        print(confusion_matrix(y_test, y_pred))
+
+        # show the metrics plot
+        print(f'\nMetrics Plot:')
+        ax = lgb.plot_metric(self.evals_result)
+        plt.show()
+
+        # show the importance plot
+        print(f'\nFeature Importance Plot:')
+        ax = lgb.plot_importance(self.model)
+        plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="ML model for Mark classification"
+    )
+    parser.add_argument(
+        '-f',
+        '--file',
+        dest='csv_file',
+        type=str,
+        help='Path to the CSV file'
+    )
+    args = parser.parse_args()
+
+    ml_model = MachineLearningModel(args.csv_file)
+    ml_model.preprocess_data()
+    ml_model.train()
+    ml_model.show_summary()
+    #accuracy = ml_model.evaluate()
+    #print(f'Accuracy: {accuracy:.2f}')
+
