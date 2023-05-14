9a22b81a5652240100cf8292d43ab20f379ecfad
Ryoichi Kakehi <ryoichikakehi@gmail.com>
2023-05-14 14:00:49 +0900

    new file:   show_tree.py 	new file:   tree_0.png 	new file:   tree_100.png 	new file:   tree_125.png 	new file:   tree_150.png 	new file:   tree_175.png 	new file:   tree_199.png 	new file:   tree_25.png 	new file:   tree_50.png 	new file:   tree_75.png

---
+++ GLCM/show_tree.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pickle
+
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+from onnxmltools import convert_lightgbm
+from onnxmltools.utils import save_model
+
+
+class ShowTree:
+
+    def __init__(self,
+                 model_path:str,
+                 encoder_path:str) -> None:
+
+        self.model = lgb.Booster(model_file=model_path)
+        with open(encoder_path, 'rb') as f:
+            self.encoder = pickle.load(f)
+
+    def plot_tree(self, 
+                  figsize:tuple=(20,8)):
+        for tree_index in [0, 25, 50, 75, 100, 125, 150, 175, 199]:
+            print(f"Plotting tree {tree_index}")
+            lgb.plot_tree(self.model, tree_index=tree_index, figsize=figsize)
+            plt.title(f"Tree {tree_index}")
+            plt.savefig(f"tree_{tree_index}.png")
+
+    def convert_model(self,
+                      fout:str="model.onnx"):
+        self.onnx_model = convert_lightgbm(self.model)
+        save_model(onnx_model, fout)
+
+
